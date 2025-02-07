64be89557a66c5cbae57499fac33910d351c165e
ronspider <124790265+ronspider@users.noreply.github.com>
2025-02-07 15:46:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,30 +2,6 @@ import datetime
 import ipaddress
 from ping3 import ping
 
-subnet = '10.10.61.0/24'
-output_file = 'ping_results_single.txt'
-
-def ping_sweep(subnet):
-    network = ipaddress.ip_network(subnet, strict=True)
-
-    with open(output_file, 'a') as f:
-        for ip in network.hosts():
-            ip_str = str(ip)
-            timestamp = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            message = ""
-            try:
-                response_time = ping(ip_str, timeout=1)
-                if response_time is not None:
-                    message = f"{timestamp} - {ip_str} is reachable"
-                else:
-                    message = f"{timestamp} - {ip_str} is unreachable"
-            except Exception:
-                message = f"{timestamp} - Error pinging {ip_str}"
-            print(message)
-            f.write(message + '\n')
-
-ping_sweep(subnet)
-
 
 
 '''
