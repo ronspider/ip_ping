6df5cfceaa235833c0908b764012a5b64c243a99
ronspider <rspaida@gmail.com>
2025-02-07 15:48:36 -0500

    Updated main.py

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from ping3 import ping
 '''
 https://www.packetswitch.co.uk/how-to-ping-sweep-your-network-with-python/
 https://www.freecodecamp.org/news/subnet-cheat-sheet-24-subnet-mask-30-26-27-29-and-other-ip-address-cidr-network-references/
-,,,
+'''
