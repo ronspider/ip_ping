b90275b4f48f2fc42215104c21720ab907f97e42
ronspider <rspaida@gmail.com>
2025-02-07 19:27:25 -0500

    Edit main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
-import datetime
-import ipaddress
-from ping3 import ping
+from ping3 import ping, verbose_ping
 
 
+sub_ran_min = 130
+sub_ran_max = 140
+ip_add = "192.168.12."
+
+sub_range = [x for x in range(sub_ran_min,sub_ran_max)]
+
+for ip in sub_range:
+    search_ip = ip_add + str(ip)
+    #result = ping(search_ip)
+    print(search_ip)
+
+ping("192.168.12.12")
 
 '''
 https://www.packetswitch.co.uk/how-to-ping-sweep-your-network-with-python/
