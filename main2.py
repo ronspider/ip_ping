f82f71c16d32b324a00e6616409113a54d98efae
ronspider <rspaida@gmail.com>
2025-02-13 12:16:35 -0500

    Cleaned up folder and main2.py

--- main2.py
+++ main2.py
@@ -1,6 +1,15 @@
-from ping3 import ping, verbose_ping
+from ping3 import ping # verbose_ping
 import openpyxl
 from IPy import IP
+from datetime import datetime
+
+
+now_date = datetime.today().strftime('%Y%m%d')
+now_time = datetime.today().strftime('%H:%M:%S')
+
+print(now_date)
+print(now_time)
+
 
 # Function
 def ip_scan(search_ip):
@@ -9,15 +18,18 @@ def ip_scan(search_ip):
     try:
         IP(search_ip)
         if result_type == type(None):
-            return f"wasn't reached."
+            print("no")
+            return "no"
 
         elif result_type is not bool:
-            return f"was reached in {round(result,6)} seconds."
+            print(f"{round(result,5)} sec")
+            return f"{round(result,5)} s"
 
         else:
-            return f"wasn't reached."
+            return f"no"
     except:
-        print("no ip found")
+        print("no ip")
+        return "no ip"
 
 
 # Open source workbook
@@ -30,6 +42,12 @@ worksheet = wb_source.active
 scan_range = worksheet.max_row + 1
 # scan_range = 15
 
+# Header information for new columns
+result_header_1 = f"IP Scan Time on {datetime.today().strftime('%Y-%m-%d')}"
+result_header_2 = "Response time"
+worksheet["P1"].value = result_header_1
+worksheet["Q1"].value = result_header_2
+
 # Loop over the rows read Col C and write results to Col P
 for row in range(2,scan_range):
     for column in "C":
@@ -37,9 +55,14 @@ for row in range(2,scan_range):
         string_ip = str(worksheet[cell_name].value)
         print(f"Scanning... {string_ip}")
         cell_result = ip_scan(string_ip)
+        
         for column in "P":
-            new_cell_name = "{}{}".format(column,row)
-            worksheet[new_cell_name].value = cell_result
-
+            new_cell_name_1 = "{}{}".format(column,row)
+            worksheet[new_cell_name_1].value = datetime.today().strftime('%H:%M:%S')
+        
+        for column in "Q":
+            new_cell_name_2 = "{}{}".format(column,row)
+            worksheet[new_cell_name_2].value = cell_result
+           
 # Save current workbook in a copy
-wb_source.save("file_output.xlsx")
+wb_source.save(f"{now_date}_{file_name}")
